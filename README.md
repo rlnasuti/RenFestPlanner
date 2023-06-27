3d67972869a92e493c810c16aebc258a983c78f7
Robert Nasuti <rlnasuti@hotmail.com>
2023-06-26 19:06:53 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # RenFestPlanner
 A bot that will look at a Renaissance Festival flyer and generate an itinerary for the user
+
+To run:
+
+1. install required libraries from requirements.txt
+1. create a file in the root of this repo called `.env`
+1. populate required fields in .env
+```
+OPENAI_API_KEY="<your openai api key"
+PDF_FILE_PATH="location of Renaissance Faire program pdf on your computer"
+```
---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+import PyPDF2
+import openai
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Load the pdf
+pdf_file_path = os.getenv('PDF_FILE_PATH')
+pdf_reader = PyPDF2.PdfReader(pdf_file_path)
+
+# Read the content
+content = ""
+for page in pdf_reader.pages:
+    content += page.extract_text()
+
+# Setup OpenAI
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# Define your prompt
+prompt = content + "\nGiven the above, generate an itinerary for a visitor."
+
+# Use the OpenAI API
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt},
+    ]
+)
+
+# Print the generated schedule
+print(response['choices'][0]['message']['content'])
